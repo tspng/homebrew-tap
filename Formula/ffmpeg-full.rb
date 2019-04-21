fdbe694a6170b98f333a33a4e58c6b07e56161b3
tspng <tspng@socket.ch>
2019-04-21 15:16:16 +0200

    Add ffmpeg-full with libfdk support

---
+++ Formula/ffmpeg-full.rb
@@ -0,0 +1,104 @@
+class FfmpegFull < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-4.1.3.tar.xz"
+  sha256 "0c3020452880581a8face91595b239198078645e7d7184273b8bcc7758beb63d"
+  head "https://github.com/FFmpeg/FFmpeg.git"
+
+  bottle do
+    sha256 "14a4dc07d1ffb6d44d90adb72185434d6c0127cf26a8ebd05ca64fa2c4baca56" => :mojave
+    sha256 "761b20937690043ced7797a18538e5adf32cda5d557810dca030ddd0cd4cd5ec" => :high_sierra
+    sha256 "8bf6a1fda6bc5adcb6dc1b89e1886dbdb2a8730d146d7fdd7ed43f4dee07d663" => :sierra
+  end
+
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texi2html" => :build
+
+  depends_on "aom"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "gnutls"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libbluray"
+  depends_on "libsoxr"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "opencore-amr"
+  depends_on "openjpeg"
+  depends_on "opus"
+  depends_on "rtmpdump"
+  depends_on "rubberband"
+  depends_on "sdl2"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "tesseract"
+  depends_on "theora"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xvid"
+  depends_on "xz"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-nonfree
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-hardcoded-tables
+      --enable-avresample
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gnutls
+      --enable-gpl
+      --enable-libaom
+      --enable-libbluray
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-librubberband
+      --enable-libsnappy
+      --enable-libtesseract
+      --enable-libtheora
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libx264
+      --enable-libx265
+      --enable-libxvid
+      --enable-lzma
+      --enable-libfdk-aac
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-libopenjpeg
+      --enable-librtmp
+      --enable-libspeex
+      --enable-videotoolbox
+      --disable-libjack
+      --disable-indev=jack
+      --enable-libaom
+      --enable-libsoxr
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# tspng's Homebrew Tap
+
+Custom formulae for the Homebrew package manager.
+
+## How do I install these formulae?
+
+Just `brew tap tspng/tap`.
